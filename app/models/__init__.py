9439121bece20f398f0640a58d1e4d60961d3089
Shanujan <shanushanujan234@gmail.com>
2026-07-15 10:47:27 +0530

    all models exposed in init file

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,37 @@
 from app.models.user_model import User
+from app.models.company_model import Company
+from app.models.skill_model import Skill
+from app.models.user_skill_model import UserSkill
+from app.models.interest_model import Interest
+from app.models.user_interest_model import UserInterest
+from app.models.job_model import Job
+from app.models.job_skill_model import JobSkill
+from app.models.application_model import Application
+from app.models.post_model import Post
+from app.models.comment_model import Comment
+from app.models.report_model import Report
+from app.models.mentorship_model import Mentorship
+from app.models.conversation_model import Conversation
+from app.models.message_model import Message
+from app.models.saved_job_model import SavedJob
+from app.models.notification_model import Notification
 
-__all__ = ["User"]
+__all__ = [
+    "User",
+    "Company",
+    "Skill",
+    "UserSkill",
+    "Interest",
+    "UserInterest",
+    "Job",
+    "JobSkill",
+    "Application",
+    "Post",
+    "Comment",
+    "Report",
+    "Mentorship",
+    "Conversation",
+    "Message",
+    "SavedJob",
+    "Notification",
+]
