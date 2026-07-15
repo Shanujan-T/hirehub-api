607e273d0096baaa78181ef9c9ad5f2275f7efe6
Shanujan <shanushanujan234@gmail.com>
2026-07-15 10:47:24 +0530

    notification model schema skeleton created

---
+++ app/models/notification_model.py
@@ -0,0 +1,14 @@
+from app.extensions import db
+from app.utils import utc_now
+
+
+class Notification(db.Model):
+    __tablename__ = "notifications"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    type = db.Column(db.String(50), nullable=False)
+    message = db.Column(db.String(500), nullable=False)
+    link_url = db.Column(db.String(500), nullable=True)
+    is_read = db.Column(db.Boolean, nullable=False, default=False)
+    created_at = db.Column(db.DateTime, default=utc_now)
