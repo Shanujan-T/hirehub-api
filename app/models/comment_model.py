329fcec400d12021f8160f6f4395343bf73a32c1
Shanujan <shanushanujan234@gmail.com>
2026-07-15 10:47:23 +0530

    comment model relationships and methods implemented

--- app/models/comment_model.py
+++ app/models/comment_model.py
@@ -10,3 +10,22 @@ class Comment(db.Model):
     author_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     body = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, default=utc_now)
+
+    post = db.relationship("Post", back_populates="comments")
+    author = db.relationship("User", back_populates="comments", foreign_keys=[author_id])
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "post_id": self.post_id,
+            "author_id": self.author_id,
+            "body": self.body,
+            "author": {
+                "id": self.author.id,
+                "full_name": self.author.full_name,
+                "avatar_url": self.author.avatar_url,
+            }
+            if self.author
+            else None,
+            "created_at": self.created_at.isoformat() if self.created_at else None,
+        }
