7864e480b24e561b38bc33066fb16328ae643371
Shanujan <shanushanujan234@gmail.com>
2026-07-22 21:46:03 +0530

    run seeders quiz step added

--- run_seeders.py
+++ run_seeders.py
@@ -2,7 +2,7 @@ from app.seeders.admin_seeder import seed_admin
 from app.seeders.community_content_seeder import seed_community_content
 from app.seeders.demo_seeder import seed_demo
 from app.seeders.image_posts_seeder import seed_image_posts
-from app.seeders.jobs_seeder import seed_jobs
+from app.seeders.quiz_seeder import seed_skill_quizzes
 
 
 def run_all():
@@ -16,6 +16,8 @@ def run_all():
     seed_community_content()
     print("Running image posts seeder...")
     seed_image_posts()
+    print("Running skill quiz seeder...")
+    seed_skill_quizzes()
 
 
 if __name__ == "__main__":
